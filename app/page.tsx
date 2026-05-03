0a2dfc17599d0aa633bbd92099815f2c51d33f31
tiw25999 <tiw25999@gmail.com>
2026-05-03 16:27:13 +0700

    fix: force-dynamic on homepage to prevent build-time DB query

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-﻿import Image from "next/image";
+﻿export const dynamic = "force-dynamic";
+
+import Image from "next/image";
 import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 
